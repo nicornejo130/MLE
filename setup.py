0e1dad84296b4d091c3c002352e71fc60e36de0e
nicornejo130 <nicornejo130@gmail.com>
2026-05-09 03:03:11 -0400

    fix: add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="flight-delay-challenge",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.10,<3.11",
+)
